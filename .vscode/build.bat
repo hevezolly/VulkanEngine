4fa2985afdfb2fa32897c7a630c0df86ee039ff1
seva <sevafinogenov@gmail.com>
2025-11-23 13:50:57 +0200

    build config

---
+++ .vscode/build.bat
@@ -0,0 +1,8 @@
+@echo off
+set build_path="%1\\build"
+cmake -S %1 -B %build_path% -DCMAKE_BUILD_TYPE=%2
+cmake --build %build_path% --config %2
+
+IF not %ERRORLEVEL% EQU 0 exit /b %ERRORLEVEL%
+
+IF "%3"=="-e" %build_path%\\%2\\bin\\examples\\window\\window.exe
