423b8d828f556ce1ca060252db786542606ce1d5
hevezolly <sevafinogenov@gmail.com>
2025-11-23 19:38:10 +0200

    more graphics pipeline

--- include/core/pipelines/graphics_pipeline.h
+++ include/core/pipelines/graphics_pipeline.h
@@ -4,16 +4,34 @@
 #include <render_context.h>
 #include <shader_source.h>
 #include <volk.h>
+#include <optional>
 
 struct API GraphicsPipelineBuilder {
     
     std::vector<VkShaderModule> shaderModules;
     std::vector<VkPipelineShaderStageCreateInfo> stages;
+    std::vector<VkDynamicState> dynamicStates;
+    std::optional<VkViewport> viewport;
+    std::optional<VkRect2D> scissor;
+    VkPipelineInputAssemblyStateCreateInfo inputAssembly;
+    VkPipelineRasterizationStateCreateInfo rasterization;
 
     GraphicsPipelineBuilder(RenderContext& context);
 
     GraphicsPipelineBuilder& AddShaderStage(ShaderStage stage, ShaderBinary& binary);
 
+    GraphicsPipelineBuilder& AddDynamicState(VkDynamicState state);
+
+    GraphicsPipelineBuilder& SetViewport(VkViewport viewport);
+    
+    GraphicsPipelineBuilder& SetScissor(VkRect2D scissor);
+
+    GraphicsPipelineBuilder& SetTopology(
+        VkPrimitiveTopology topology, bool restartEnabled=false);
+
+    GraphicsPipelineBuilder& SetCullMode(
+        VkCullModeFlagBits mode, VkFrontFace frontFace = VK_FRONT_FACE_CLOCKWISE
+    );
 
 private:
     RenderContext* context;
--- src/core/pipelines/graphics_pipeline.cpp
+++ src/core/pipelines/graphics_pipeline.cpp
@@ -2,9 +2,22 @@
 
 GraphicsPipelineBuilder::GraphicsPipelineBuilder(RenderContext& context) {
     this->context = &context;
+    inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+    inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    inputAssembly.primitiveRestartEnable = VK_FALSE;
+
+    rasterization.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    rasterization.depthClampEnable = VK_FALSE;
+    rasterization.depthBiasEnable = VK_FALSE;
+    rasterization.lineWidth = 1.f;
+    rasterization.cullMode = VK_CULL_MODE_BACK_BIT;
+    rasterization.frontFace = VK_FRONT_FACE_CLOCKWISE;
 }
 
-GraphicsPipelineBuilder& GraphicsPipelineBuilder::AddShaderStage(ShaderStage stage, ShaderBinary& binary) {
+GraphicsPipelineBuilder& GraphicsPipelineBuilder::AddShaderStage(
+    ShaderStage stage, 
+    ShaderBinary& binary
+) {
     VkShaderModule vkModule;
     VkShaderModuleCreateInfo moduleInfo{VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO};
     moduleInfo.codeSize = binary.size_in_bytes();
@@ -21,5 +34,38 @@ GraphicsPipelineBuilder& GraphicsPipelineBuilder::AddShaderStage(ShaderStage sta
 
     stages.push_back(stageInfo);
 
+    return *this;
+}
+
+GraphicsPipelineBuilder& GraphicsPipelineBuilder::AddDynamicState(VkDynamicState state) {
+    dynamicStates.push_back(state);
+    return *this;
+}
+
+GraphicsPipelineBuilder& GraphicsPipelineBuilder::SetViewport(VkViewport viewport) {
+    this->viewport = viewport;
+    return *this;
+}
+
+GraphicsPipelineBuilder& GraphicsPipelineBuilder::SetScissor(VkRect2D scissor) {
+    this->scissor = scissor;
+    return *this;
+}
+
+GraphicsPipelineBuilder& GraphicsPipelineBuilder::SetTopology(
+    VkPrimitiveTopology topology, 
+    bool restartEnabled=false
+) {
+    inputAssembly.topology = topology;
+    inputAssembly.primitiveRestartEnable = restartEnabled ? VK_TRUE : VK_FALSE;
+    return *this;
+}
+
+GraphicsPipelineBuilder& GraphicsPipelineBuilder::SetCullMode(
+    VkCullModeFlagBits mode, 
+    VkFrontFace frontFace = VK_FRONT_FACE_CLOCKWISE
+) {
+    rasterization.cullMode = mode;
+    rasterization.frontFace = frontFace;
     return *this;
 }
